9926695ec2e4e3af2f278174be1340dd7eb33bb0
hartono <harutonouciha@gmail.com>
2022-06-15 14:28:00 +0700

    initial commit

---
+++ com_read/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_read = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.btn_stop = New System.Windows.Forms.Button()
+        Me.cmd_txt = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'btn_read
+        '
+        Me.btn_read.Location = New System.Drawing.Point(263, 45)
+        Me.btn_read.Name = "btn_read"
+        Me.btn_read.Size = New System.Drawing.Size(75, 23)
+        Me.btn_read.TabIndex = 0
+        Me.btn_read.Text = "Read"
+        Me.btn_read.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "COM Port"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.HorizontalScrollbar = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 93)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(555, 212)
+        Me.ListBox1.TabIndex = 3
+        '
+        'btn_stop
+        '
+        Me.btn_stop.Location = New System.Drawing.Point(363, 45)
+        Me.btn_stop.Name = "btn_stop"
+        Me.btn_stop.Size = New System.Drawing.Size(75, 23)
+        Me.btn_stop.TabIndex = 4
+        Me.btn_stop.Text = "Stop"
+        Me.btn_stop.UseVisualStyleBackColor = True
+        '
+        'cmd_txt
+        '
+        Me.cmd_txt.Location = New System.Drawing.Point(82, 47)
+        Me.cmd_txt.Name = "cmd_txt"
+        Me.cmd_txt.Size = New System.Drawing.Size(146, 20)
+        Me.cmd_txt.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(19, 50)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Command"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"COM1", "COM2", "COM3", "COM4", "COM5", "COM6"})
+        Me.ComboBox1.Location = New System.Drawing.Point(82, 10)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(579, 311)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.cmd_txt)
+        Me.Controls.Add(Me.btn_stop)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btn_read)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_read As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents btn_stop As Button
+    Friend WithEvents cmd_txt As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+End Class
---
+++ com_read/Form1.vb
@@ -0,0 +1,86 @@
+﻿Imports System.IO.Ports
+
+Public Class Form1
+    Dim baca As Boolean = True
+    Private Sub btn_read_Click(sender As Object, e As EventArgs) Handles btn_read.Click
+        If ComboBox1.Text = "" Or cmd_txt.Text = "" Then
+            MsgBox("COM Port belum diisi !")
+            Exit Sub
+        End If
+        Dim port As String = ComboBox1.Text
+        Dim cmd As String = cmd_txt.Text
+
+        Dim returnStr As String = ""
+
+        'Dim com1 As IO.Ports.SerialPort = Nothing
+
+        'com1 = My.Computer.Ports.OpenSerialPort(port)
+        Dim com1 As SerialPort
+        com1 = New SerialPort(port, 2400, Parity.None, 7, StopBits.One)
+        If com1.IsOpen Then
+            com1.Close()
+        End If
+        Try
+            com1.Open()
+            com1.WriteTimeout = 10000
+            com1.WriteLine(cmd)
+
+        Catch ex As Exception
+            returnStr = ex.ToString()
+            ListBox1.Items.Add(returnStr)
+            com1.Close()
+            Exit Sub
+        End Try
+        Try
+            'com1 = My.Computer.Ports.OpenSerialPort(port)
+
+            com1.ReadTimeout = 10000
+
+
+            Dim Incoming As String = com1.ReadLine()
+
+            'returnStr &= Incoming & vbCrLf
+            ListBox1.Items.Add(Incoming & vbCrLf)
+
+        Catch ex As Exception
+            returnStr = ex.ToString()
+            ListBox1.Items.Add(returnStr)
+        Finally
+            If com1 IsNot Nothing Then com1.Close()
+        End Try
+
+    End Sub
+    Function ReceiveSerialData(ByVal comport As String) As String
+        ' Receive strings from a serial port.
+        Dim returnStr As String = ""
+
+        Dim com1 As IO.Ports.SerialPort = Nothing
+        Try
+            com1 = My.Computer.Ports.OpenSerialPort(comport)
+            com1.ReadTimeout = 10000
+            Do
+                Dim Incoming As String = com1.ReadLine()
+                If baca = False Then
+                    com1.Close()
+                    Exit Do
+                End If
+                If Incoming Is Nothing Then
+                    Exit Do
+                Else
+                    returnStr &= Incoming & vbCrLf
+                End If
+            Loop
+        Catch ex As Exception
+            returnStr = ex.ToString()
+        Finally
+            If com1 IsNot Nothing Then com1.Close()
+        End Try
+
+        Return returnStr
+    End Function
+
+    Private Sub btn_stop_Click(sender As Object, e As EventArgs) Handles btn_stop.Click
+
+        Close()
+    End Sub
+End Class
